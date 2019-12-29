89d3789129e6c148a27deb576c46ac7cabaa2fd8
jsz <shangj3@vt.edu>
2019-12-28 17:04:50 -0800

    new

--- autoAim/autoAim.cpp
+++ autoAim/autoAim.cpp
@@ -11,9 +11,6 @@ double calc_distance(Point2f p1, Point2f p2) {
     return pow(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2), 0.5);
 }
 
-int armor::get_average_intensity(const cv::Mat &img) {
-    return 0;
-}
 
 cv::Rect ArmorDetector::GetRoi(const cv::Mat &img) {
     Size img_size = img.size();
@@ -48,32 +45,129 @@ cv::Rect ArmorDetector::GetRoi(const cv::Mat &img) {
     return rect_roi;
 }
 
-int ArmorDetector::DetectArmor(cv::Mat& img, cv::Point3f &target_3d, cv::Mat &depth_img, cv::Rect roi)
-{
+bool ArmorDetector::DetectArmor(cv::Mat &img, cv::Point3f &target_3d, cv::Mat &depth_img, cv::Rect roi) {
     Mat roi_image = img(roi);
-    Point2f  offset_roi_point(roi.x,roi.y);
+    Point2f offset_roi_point(roi.x, roi.y);
     vector<LED_bar> LED_bars;
     Mat binary_brightness_img, binary_color_img, gray;
-    cvtColor(roi_image,gray,COLOR_BGR2GRAY);
-    vector<Mat>  BGR_channels;
-    split(roi_image,BGR_channels);
+    cvtColor(roi_image, gray, COLOR_BGR2GRAY);
+    vector<Mat> BGR_channels;
+    split(roi_image, BGR_channels);
     Mat result_img;
     if (color_ = 0) // blue
     {
         subtract(BGR_channels[2], BGR_channels[1], result_img);
-    } else
-    {
+    } else {
         subtract(BGR_channels[0], BGR_channels[2], result_img);
     }
-    threshold(gray,binary_brightness_img,gray_th_,255, THRESH_BINARY);
+    threshold(gray, binary_brightness_img, gray_th_, 255, THRESH_BINARY);
     threshold(result_img, binary_color_img, color_th_, 255, THRESH_BINARY);
 
 #ifdef SHOW_BINART
     imshow("binary_brightness_img", binary_brightness_img);
     imshow("binary_color_img", binary_color_img);
 #endif
+    vector<vector<Point> > contours_light;
+    vector<vector<Point> > contours_brightness;
+    findContours(binary_color_img, contours_light, RETR_EXTERNAL, CHAIN_APPROX_NONE);
+    findContours(binary_brightness_img, contours_brightness, RETR_EXTERNAL, CHAIN_APPROX_NONE);
 
+    for (auto &contours_brightnes : contours_brightness) {
+        double area = contourArea(contours_brightnes);
+        if (area < 20 || area > 1e5) {
+            continue;
+        }
+        for (const auto &j : contours_light) {
+            if (pointPolygonTest(j, contours_brightnes[0], false) >= 0.0) {
+                double length = arcLength(contours_brightnes, true);
+                if (length > 15 && length < 4000) {
+                    RotatedRect RRect = fitEllipse(contours_brightnes);
+#ifdef SHOW_LIGHT_CONTOURS
+                    Point2f rect_point[4];
+                    RRect.points(rect_point);
+                    for (int i = 0; i < 4; i++) {
+                        line(img, rect_point[i] + offset_roi_point, rect_point[(i + 1) % 4] + offset_roi_point,
+                             Scalar(255, 0, 255), 1);
+                    }
+#endif
+                    if (RRect.angle > 90.0f) {
+                        RRect.angle = RRect.angle - 180.0f;
+                    }
+#ifdef SHOW_LIGHT_CONTOURS
+                    putText(img, to_string(RRect.angle), RRect.center + Point2f(2, 2) + offset_roi_point,
+                            FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255, 255, 255), 1);
+#endif
+                    if (fabs(RRect.angle) <= 30) {
+                        LED_bar r(RRect);
+                        LED_bars.emplace_back(r);
+
+                    }
+                }
+                break;
+            }
+        }
+    }
+
+    //==========================================possible armor=========================================
+
+    for (size_t i = 0; i < LED_bars.size(); i++) {
+        for (size_t j = i + 1; j < LED_bars.size(); j++) {
+            armor temp_armor(LED_bars.at(i), LED_bars.at(j));
+            if (temp_armor.error_angle < 8.0f)
+            {
+                if (temp_armor.is_suitable_size())
+                {
+                    if(temp_armor.get_average_intensity(gray)< 50 )
+                    {
+                        temp_armor.max_match(LED_bars, i, j);
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    //====================================find final armors============================================
+    vector<armor> final_armor_list;
+    for(size_t i = 0; i < LED_bars.size() ; i++)
+    {
+        if(LED_bars.at(i).matched)
+        {
+            LED_bars.at(LED_bars.at(i).match_index).matched = false; //clear another matching flag
+            armor arm_tmp( LED_bars.at(i), LED_bars.at(LED_bars.at(i).match_index));
+            final_armor_list.push_back(arm_tmp);
+        }
+    }
+
+    float dist=1e8;
+    bool found_flag = false;
+    armor target;
+    Point2f roi_center(roi.width/2, roi.height/2);
+    float dx,dy;
+    for (size_t i = 0; i < final_armor_list.size() ; i++ )
+    {
+#ifdef FAST_DISTANCE
+        dx = fabs(final_armor_list.at(i).center.x - roi_center.x);
+        dy = fabs(final_armor_list.at(i).center.y - roi_center.y);
+#else
+        dx = pow((final_armor_list.at(i).center.x - roi_center.x), 2.0f);
+        dy = pow((final_armor_list.at(i).center.y - roi_center.y), 2.0f);
+#endif
+        if( dx + dy < dist){
+            target = final_armor_list.at(i);
+            dist = dx + dy;
+        }
+#ifdef SHOW_FINAL_ARMOR
+        final_armor_list.at(i).draw_rect(img, offset_roi_point);
+#endif
+        found_flag = true;
+    }
+#ifdef SHOW_ROI
+    rectangle(img,roi,Scalar(255,0,255),1);
+#endif
 }
+
 int ArmorDetector::armorTask(cv::Mat &img, OtherParam other_param) {
     color_ = other_param.color;
     mode_ = other_param.mode;
@@ -84,6 +178,7 @@ int ArmorDetector::armorTask(cv::Mat &img, OtherParam other_param) {
     Rect roi = Rect(0,0, img_size.width, img_size.height);
 #endif
     Point3f target_3d = {};
-    DetectArmor(img,target_3d,img,roi);
+    DetectArmor(img, target_3d, img, roi);
 }
+
 
--- autoAim/autoAim.h
+++ autoAim/autoAim.h
@@ -8,52 +8,13 @@
 #include <opencv2/opencv.hpp>
 #include <stdlib.h>
 #include "../control.h"
-
+#include "armor.h"
 struct OtherParam {
     uint8_t color = 0;
     uint8_t mode = 0;
 };
 
-class LED_bar {
-public:
-    LED_bar() : matched(false) {}
 
-    LED_bar(const cv::RotatedRect &R) {
-        rect.angle = R.angle;
-        rect.center = R.center;
-        rect.size = R.size;
-        matched = false;
-    }
-
-    cv::RotatedRect rect;
-    bool matched;
-    size_t match_index;
-    float match_factor;
-};
-
-class armor {
-public:
-    armor();
-
-    armor(const LED_bar &left, const LED_bar &right);
-
-    void draw_rect();
-
-    void draw_spot();
-
-    int get_average_intensity(const cv::Mat &img);
-
-    void max_match(std::vector<LED_bar> &LEDs, size_t i, size_t j);
-
-    bool is_suitable_size;
-
-    LED_bar led_bar[2];
-    float error_angle;
-    cv::Point2i center;
-    cv::Rect2i rect;
-    int average_intensity;
-
-};
 
 class ArmorDetector {
 public:
@@ -65,18 +26,7 @@ public:
 
     int armorTask(cv::Mat &img, OtherParam other_param);
 
-    int DetectArmor(cv::Mat& img, cv::Point3f &target_3d, cv::Mat &depth_img, cv::Rect roi);
-
-    void DetectLights(const cv::Mat &src, std::vector<cv::RotatedRect> &lights);
-
-    void FilterLights(std::vector<cv::RotatedRect> &lights);
-
-    void PossibleArmors(const std::vector<cv::RotatedRect> &lights, std::vector<armor> &armors);
-
-    void FilterArmors(std::vector<armor> &armors);
-
-    armor SlectFinalArmor(std::vector<armor> &armors);
-
+    bool DetectArmor(cv::Mat& img, cv::Point3f &target_3d, cv::Mat &depth_img, cv::Rect roi);
 public:
     int color_th_ = 16;
     int gray_th_ = 60;
